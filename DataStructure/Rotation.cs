a6569e76be998d33a2644d41ecc236e7dabc988a
Ritesh Adbale <ritesh.adbale@gmail.com>
2022-03-30 22:18:45 +0530

    Add project files.

---
+++ DataStructure/Rotation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataStructure
+{
+    class Rotation
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
