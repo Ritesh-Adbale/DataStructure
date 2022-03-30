be7793a503c7d1d5bc4252f10437d69b9a2285a9
Ritesh Adbale <ritesh.adbale@gmail.com>
2022-03-30 22:39:04 +0530

    Rotation with accepting array from input

--- DataStructure/Rotation.cs
+++ DataStructure/Rotation.cs
@@ -4,9 +4,46 @@ namespace DataStructure
 {
     class Rotation
     {
+        static void leftRotate(int[] arr,int d, int n)
+        {
+            for (int i = 0; i < d; i++)
+            {
+                leftRotatebyOne(arr,n);
+            }
+        }
+
+        static void leftRotatebyOne(int[]arr,int n)
+        {
+            int i, temp = arr[0];
+            for ( i = 0; i < n-1; i++)
+            {
+                arr[i] = arr[i + 1];
+                
+            }
+            arr[n - 1] = temp;
+        }
+
+        static void printArray(int[]arr,int size)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write(arr[i]+" ");
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n;
+            n = Int32.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            string[] s = Console.ReadLine().Split(' ');
+
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = Int32.Parse(s[i]);
+            }
+            leftRotate(arr, 2, 7);
+            printArray(arr, 7);
+            Console.ReadKey();
         }
     }
 }
