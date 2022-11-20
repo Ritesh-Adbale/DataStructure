718736ecb94317e8fb38ac5a32b2473ca4f40766
Ritesh-Adbale <ritesh.adbale@gmail.com>
2022-11-20 14:53:32 +0530

    Factorial number using recursion

---
+++ DataStructure/01 Recursion/FactorialNumberN.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    public class FactorialNumberN
+    {
+        static int func(int n)
+        {
+            if (n == 0)
+                return 1;
+            int partialans = func(n - 1);
+            Console.WriteLine("n"+n);
+            Console.WriteLine("p"+partialans);
+            return n*partialans;
+
+        }
+       public static void Main()
+       {
+            Console.WriteLine(func(3));
+            Console.ReadKey();
+       }
+    }
+}
