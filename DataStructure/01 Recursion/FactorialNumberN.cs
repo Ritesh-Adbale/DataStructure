265c22dea98a35e7fd9e3cfd3b5c2870f9d548f7
Ritesh-Adbale <ritesh.adbale@gmail.com>
2022-11-21 12:12:08 +0530

    Factorial file changes

--- DataStructure/01 Recursion/FactorialNumberN.cs
+++ DataStructure/01 Recursion/FactorialNumberN.cs
@@ -1,27 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DataStructure
 {
-    public class FactorialNumberN
+    // Factorial
+    // n! = n * (n-1) * (n-2) * (n-3)….* 1
+    // 3!=3*2*1=6
+    public class FactorialNumberN 
     {
         static int func(int n)
         {
             if (n == 0)
                 return 1;
             int partialans = func(n - 1);
-            Console.WriteLine("n"+n);
-            Console.WriteLine("p"+partialans);
+            // Console.WriteLine("n"+n);
+            // Console.WriteLine("p"+partialans);
             return n*partialans;
 
         }
        public static void Main()
        {
-            Console.WriteLine(func(3));
+            Console.Write("Enter a number for Factorial :- ");
+        //Accept enter number here.
+           int number=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(number);
+        //if number is smaller than zero    
+            if(number<=0)
+            {
+                Console.Write("Error: Enter number Greater than Zero");
+            }else{
+
+            Console.WriteLine(func(number));
+            }
             Console.ReadKey();
        }
     }
 }
+/*
+    time complexity : O(n)
+    space complexity : O(n)
+*/
